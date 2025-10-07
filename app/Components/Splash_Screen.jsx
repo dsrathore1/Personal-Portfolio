bafb029cc823f1ba07a226ea0ed070e9bff1c50c
PrayagRaj22 <prayagrajsingh55@protonmail.com>
2025-10-07 15:51:16 +0530

    Update in splash screen

--- app/Components/Splash_Screen.jsx
+++ app/Components/Splash_Screen.jsx
@@ -2,21 +2,26 @@
 import React from 'react'
 import { motion } from "framer-motion"
 import Link from "next/link"
+import { Skiper58, TextRoll } from './TextRoll'
 
 const Splash_Screen = () => {
     return (
-        <div className='h-screen flex flex-col justify-center items-center bg-amber-100 gap-10'>
-            <motion.h1
-                initial={{ y: -250, opacity: 0 }}
+        <div className='flex flex-col relative justify-center items-center h-screen w-full bg-amber-100'>
+            <motion.div initial={{ y: -250, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                transition={{ type: "spring", stiffness: 120, duration: 1.5 }}
-                className='text-9xl text-white font-bold font-mono cursor-default select-none'>HeLlo WoRlD</motion.h1>
+                transition={{ type: "spring", stiffness: 120, duration: 1.5 }}>
+                <TextRoll />
+                <Skiper58 />
+            </motion.div>
             <motion.div
-                className="h-[3rem] w-[10rem] bg-amber-200 cursor-pointer rounded"
+                initial={{ x: -250, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                // transition={{ type: "spring", stiffness: 120, duration: 1.5 }}
+                className="absolute bottom-[10rem] cursor-pointer rounded"
                 whileHover={{ scale: 1.4 }}
                 transition={{ type: "spring", stiffness: 400, damping: 10 }}
             >
-                <Link className='flex justify-center items-center h-full w-full bg-amber-700 rounded-lg shadow text-white' href={"/Pages/home"}>Let's Go</Link>
+                <Link className='flex justify-center items-center h-12 w-32 bg-amber-700 rounded shadow text-white' href={"/Pages/home"}>Let's Go</Link>
             </motion.div>
         </div>
     )
