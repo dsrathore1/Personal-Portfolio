444cc7aff6e4235790c3af8950a81bad7f651c84
PrayagRaj22 <prayagrajsingh55@protonmail.com>
2025-10-07 15:50:44 +0530

    Decrease the width

--- app/Components/Nav_Header.jsx
+++ app/Components/Nav_Header.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Nav_Header = () => {
     return (
-        <div className='flex text-gray-700 bg-[#faf9f4] justify-between text-[.9rem] items-center border-b-[.05rem] border-gray-200 shadow-xs h-[4rem] w-screen absolute z-[1]'>
+        <div className='flex text-gray-700 bg-[#faf9f4] justify-between text-[.9rem] items-center border-b-[.05rem] border-gray-200 shadow-xs h-[4rem] w-full absolute z-[1]'>
             <div className='ml-10'>
                 <Link href={"/"} className='text-black font-bold text-[1.2rem] font-sans cursor-pointer select-none tracking-wider'>portfolio.</Link>
             </div>
@@ -14,7 +14,7 @@ const Nav_Header = () => {
                     <li className='hover:text-black hover:font-bold duration-200 ease-in-out cursor-pointer'><Link href="/Pages/blog">Blog</Link></li>
                     <li className='hover:text-black hover:font-bold duration-200 ease-in-out cursor-pointer'><Link href="/Pages/work">Work</Link></li>
                     <li className='hover:text-black hover:font-bold duration-200 ease-in-out cursor-pointer'><Link href="/Pages/toolkit">Toolkit</Link></li>
-                    <li className='hover:text-black hover:font-bold duration-200 ease-in-out cursor-pointer'><Link href="/Pages/photography">Photography</Link></li>
+                    <li className='hover:text-black hover:font-bold duration-200 ease-in-out cursor-pointer'><Link href="/Pages/photograph">Photography</Link></li>
                     <li className='hover:text-black hover:font-bold duration-200 ease-in-out cursor-pointer'><Link href="/Pages/cv">CV</Link></li>
                 </ul>
             </div>
