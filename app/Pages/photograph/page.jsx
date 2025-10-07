7bff529e17e80c63f484d32e641ec0824a500478
PrayagRaj22 <prayagrajsingh55@protonmail.com>
2025-10-07 15:52:14 +0530

    Introduce the photography section

---
+++ app/Pages/photograph/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Skiper30 } from './imageLoader'
+
+const Photograph = () => {
+    return (
+        <>
+            <Skiper30 />
+        </>
+    )
+}
+
+export default Photograph
